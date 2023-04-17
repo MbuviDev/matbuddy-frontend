7a097869b7e814b70b4026b342048d2d81c50241
Meshack <mbuvi.meshack99@gmail.com>
2023-04-17 06:20:07 +0300

    Login Page

---
+++ src/components/Subheading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Subheading () {
+    return (
+        <div class="text-5xl p-36 font-serif mb-8 text-white  italic text-center">
+            <h1>Your Reliable Transport Partner</h1>
+
+        </div>
+    )
+}
+
+
+export default Subheading;
