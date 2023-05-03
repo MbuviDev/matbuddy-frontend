016227d767553d15bf03bb9ab940e4dc0adae51e
Marucha Chiveli <marucha.webdev@yahoo.com>
2023-05-04 01:35:13 +0300

    feat: Expenses element, scaffold for form with a bit of styling

--- src/PAGES/Expenses/Expenses.jsx
+++ src/PAGES/Expenses/Expenses.jsx
@@ -1,36 +1,81 @@
-import React, { useRef, useState } from "react";
-import ExpensesInputs from "./Inputs/ExpensesInputs";
+// import React, { useRef, useState } from "react";
+
+function ExpensesForm(){
+    const optionList = ["Fuel", "Parking Fees", "Garage Payment", "Police Officer Stop"];
+    const vehicleList = ["KAY 567C", "KAU 128F", "KDD 231C", "KDA 980P"];
+    // className="form-input w-full block border rounded-lg text-black"
+
+    return (
+    <div className="p-2 m-2 flex flex-col gap-4 ">
+        <h2> New Expense Log </h2>
+        <form className="grid p-2 gap-4 h-full bg-white border rounded-lg shadow-xl">
+            <label className="block text-left align-middle gap-6 w-full p-2">
+                Expense Type :
+                <select 
+                    className="w-full block form-input bg-slate-100 border-none focus:border-green-500 rounded" 
+                >
+                    <option value="">
+                        -- select an expense type --
+                    </option>
+                    {
+                        optionList.map((option) => {
+                            return (
+                                <option
+                                    key={optionList.indexOf(option) + 1}
+                                    value={option}
+                                    className=""
+                                    >
+                                    {option}
+                                </option>
+                        )
+                    })}
+                </select>
+            </label>
+
+            <label className="block text-left align-middle gap-6 w-full p-2">
+                Vehicle :
+                <select className="form-input bg-slate-100">
+                    <option value="">
+                        -- select your vehicle --
+                    </option>
+                    {
+                        vehicleList.map((vehicle) => {
+                            return (
+                                <option
+                                    key={vehicleList.indexOf(vehicle) + 1}
+                                    value={vehicle}
+                                    className="bg-transparent p-2"
+                                    >
+                                        {vehicle}
+                                    </option>
+                            )
+                        })
+                    }
+                </select>
+            </label>
+
+            <label className="block text-left align-middle gap-6 w-full p-2">
+                Amount Spent (Ksh) :
+                <input type="number" min={0} max={25000}/>
+            </label>
+
+            <input 
+                type="submit" 
+                text="Create Expense"
+            />
+       </form>
+    </div>
+    )
+}
 
 
 function Expenses () {
-    const [showError, setShowError] = useState(0);
-    const input = useRef({ form: {}, err: {} });
-
-    function handleSubmit (e) {
-        e.preventDefault()
-    }
-
-    return (
-        <div className="bg-gray-300 h-screen">
-            <h1>Expense Tracker  </h1>
-            <div className="p-8 ">
-           <form onSubmit={handleSubmit} >
-           <div className="rounded-lg bg-white">
-            <h1 className="p-4">New Expense Log</h1>
-            <ExpensesInputs  input={input} showError={showError} />
-            <button type="submit" class="grid justify-items-end mb-6 mt-6 bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-700 ">Submit</button>
-           </div>
-           
-                
-           </form> 
-           <div className="bg-white p-4">
-            <h1> Expenses Breakdown</h1>
-            <p>Petrol</p>
-            <p>Diesel</p>
-           </div>
-           </div>
-        </div>
-    )
+   return (
+    <section className="p-2 font-mono text-left bg-slate-200 h-screen">
+        <h1 className="text-4xl p-2">Expense Tracker</h1>
+        <ExpensesForm />
+    </section>
+   )
 }
 
 export default Expenses;
