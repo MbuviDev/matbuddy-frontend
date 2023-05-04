7a928d667fc8a31fb863d1b7c89ae75e13c0bc3c
Marucha Chiveli <marucha.webdev@yahoo.com>
2023-05-04 04:32:13 +0300

    feat: Display Breakdown Component

--- src/PAGES/Expenses/Expenses.jsx
+++ src/PAGES/Expenses/Expenses.jsx
@@ -1,14 +1,20 @@
 // import React, { useRef, useState } from "react";
 
 import ExpensesForm from "./Components/ExpenseForm";
+import ExpensesBreakdown from "./Components/BreakDown";
 
 // Exported element is expenses , nested within can be all components for the page
 
 function Expenses () {
    return (
     <section className="p-2 font-mono text-left bg-slate-300 h-screen">
-        <h1 className="text-4xl p-2 m-2 font-bold">Expense Tracker</h1>
-        <ExpensesForm />
+        <section className="p-2 m-2">
+            <h1 className="text-4xl p-2 m-2 font-bold">Expense Tracker</h1>
+            <div className="grid gap-y-8">
+                <ExpensesForm />
+                <ExpensesBreakdown />
+            </div>
+        </section>
     </section>
    )
 }
