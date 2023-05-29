fea45855f1a54021cbed05372a494494b72f12a0
Meshack <mbuvi.meshack99@gmail.com>
2023-05-29 14:56:44 +0300

    updated expenses

---
+++ src/PAGES/Expenses/Components/ExpensesForm.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import SelectWithSearch from "../../../components/InputComponents/SelectWithSearch";
+import TextInput from "../../../components/InputComponents/textInput";
+
+function ExpensesForm({ input, showError }) {
+
+
+  return (
+    // <div className="p-2 m-4 flex flex-col gap-4 bg-white border rounded-md  shadow-slate-600 ">
+    //   <h2 className="text-2xl m-2 p-2 font-serif"> New Expense Log </h2>
+    //   <form
+    //     className="grid grid-cols-2 p-2 gap-4 h-full w-full
+	// 				align-middle items-center justify-center"
+    //   >
+		<div>
+        <SelectWithSearch
+          initial={""}
+          input={input}
+          showError={showError}
+          inputKey="destination"
+          mt={0.5}
+          // options={dummyOptions.map(obj=>({label: obj.name.toLocaleUpperCase(), value: obj._id}))}
+          config={{
+            required: true,
+            label: "Expense Type",
+            placeholder: "Select Type",
+            maxChar: 250,
+          }}
+        />
+
+        <SelectWithSearch
+          initial={""}
+          input={input}
+          showError={showError}
+          inputKey="destination"
+          mt={0.5}
+          // options={dummyOptions.map(obj=>({label: obj.name.toLocaleUpperCase(), value: obj._id}))}
+          config={{
+            required: true,
+            label: "Vehicle",
+            placeholder: "Select Vehicle",
+            maxChar: 250,
+          }}
+        />
+			 
+			
+        <TextInput
+          initial={""}
+          input={input}
+          showError={showError}
+          inputKey=""
+          mt={0.5}
+          config={{
+            required: true,
+            label: "Amount Spent(ksh)",
+            placeholder: "Enter Amount",
+            maxChar: 25,
+            minChar: 1,
+          }}
+        />
+
+        <TextInput
+          initial={""}
+          input={input}
+          showError={showError}
+          inputKey=""
+          mt={0.5}
+          config={{
+            required: true,
+            label: "Description",
+            placeholder: "Enter Description (E.g num of litres,etc)",
+            maxChar: 25,
+            minChar: 1,
+          }}
+        />
+
+       
+     
+    </div>
+  );
+}
+
+export default ExpensesForm;
--- src/PAGES/Expenses/Expenses.jsx
+++ src/PAGES/Expenses/Expenses.jsx
@@ -1,33 +1,34 @@
+import React from "react";
 import { useRef, useState } from "react";
-import ExpensesForm from "./Components/ExpenseForm";
-// import ExpensesBreakdown from "./Components/BreakDown";
-// import ExpensesInputs from "./Inputs/ExpensesInputs";
+import ExpensesForm from "./Components/ExpensesForm";
+
+
 
 // Exported element is expenses , nested within can be all components for the page
 
-function Expenses () {
-
-    const [showError] = useState[0]
-    const input = useRef({ form: {}, err: {} });
-
-    function handleSub(e){
-      e.preventDefault();
-      console.log(input.current.form)
-    }
-
-     
-    
-   return (
-    <div className="p-2 font-mono text-left bg-slate-300 min-h-screen" >
-        <div className="p-2 m-2">
-            <h1 className="text-4xl p-2 m-2 font-serif">Expense Tracker</h1>
-            <div onSubmit={handleSub} className="grid gap-y-8 ">
-                <ExpensesForm  input={input} showError={showError}/>
-                {/* <ExpensesBreakdown /> */}
+function Expenses() {
+ 
+  return (
+       
+       <div>
+        <h1 className="text-3xl p-2 m-2 font-serif">Expense Tracker</h1>
+        <div className="p-8">
+            <div  className="bg-amber-400 rounded-3xl">
+                <h1>New Expense Log</h1>
+                {/* <ExpensesForm/> */}
             </div>
         </div>
-    </div>
-   )
+       </div>
+    // <div className="p-2 font-mono text-left bg-slate-300 min-h-screen" >
+    //     <div className="p-2 m-2">
+    //         <h1 className="text-4xl p-2 m-2 font-serif">Expense Tracker</h1>
+    //         <div onSubmit={handleSub} className="grid gap-y-8 ">
+    //             <ExpensesForm  input={input} showError={showError}/>
+    //             {/* <ExpensesBreakdown /> */}
+    //         </div>
+    //     </div>
+    // </div>
+  );
 }
 
 export default Expenses;
