6b393ffc22e163687da05b6e1c00bb85ae1450ed
Meshack <mbuvi.meshack99@gmail.com>
2023-06-06 14:23:55 +0300

    added stages

---
+++ src/components/Stages/Stages.jsx
@@ -0,0 +1,189 @@
+import React from "react";
+
+function Stages () {
+
+    return (
+        <div>
+            <select >
+                            <option value="*">Select Destination</option>
+                            
+                            <option value="154423898127990784" disabled="true">AFYA CENTRE</option>
+                              
+                            <option value="373342229500002304">CHUMVI JUNCTION</option>
+                              
+                            <option value="250591605319270400">COUNTRY BUS</option>
+                              
+                            <option value="364753894372278272">EKALAKALA</option>
+                              
+                            <option value="364753769826615296">EKALALA</option>
+                              
+                            <option value="365867485859151872">ENZIU</option>
+                              
+                            <option value="169036160557907968">IKANGA</option>
+                              
+                            <option value="157757213694230528">IKUTHA</option>
+                              
+                            <option value="369456377996771328">ITANGINI</option>
+                              
+                            <option value="366567376927326208">KAANI</option>
+                              
+                            <option value="157437385200631808">KABATI</option>
+                              
+                            <option value="321518579235160064">KAMUWONGO</option>
+                              
+                            <option value="321600297317171200">KANYONYOO</option>
+                              
+                            <option value="158443052719931392">KATANGI</option>
+                              
+                            <option value="326638761024684032">KATANGINI</option>
+                              
+                            <option value="401693768425668608">KATHEANI</option>
+                              
+                            <option value="321544707563847680">KATHIVO</option>
+                              
+                            <option value="158149837886324736">KATHOZWENI</option>
+                              
+                            <option value="323377428569260032">KATSE</option>
+                              
+                            <option value="321907588549574656">KATUTU</option>
+                              
+                            <option value="325605542397476864">KAVAINI</option>
+                              
+                            <option value="372672424233140224">KAVENGE</option>
+                              
+                            <option value="157878547136905216">KIBWEZI</option>
+                              
+                            <option value="349948881326309376">KILIFI</option>
+                              
+                            <option value="321866474350182400">KILIMAMBONGO</option>
+                              
+                            <option value="324751744078708736">KIMANGAO KYUSO</option>
+                              
+                            <option value="158443457487044608">KISASI</option>
+                              
+                            <option value="154423898136379392">KITENGELA</option>
+                              
+                            <option value="160283570793873408">KITHIMANI</option>
+                              
+                            <option value="349948403850936320">KITHYOKO</option>
+                              
+                            <option value="157458692818599936">KITHYOKO-MWINGI</option>
+                              
+                            <option value="154423898153156608">KITUI</option>
+                              
+                            <option value="349948567164551168">KONGOWEA</option>
+                              
+                            <option value="161818150952763392">KONZA</option>
+                              
+                            <option value="349948232861745152">KUNDA KINDU</option>
+                              
+                            <option value="394735198484299776">KWA KONGA</option>
+                              
+                            <option value="325190321266753536">KWA MONA</option>
+                              
+                            <option value="369050032189472768">KWA MUMO</option>
+                              
+                            <option value="157775330357018624">KWA VONZA</option>
+                              
+                            <option value="399913083465629697">KWAKOLA</option>
+                              
+                            <option value="374795610697498624">KWAMUTHUSI</option>
+                              
+                            <option value="169036288702283776">KYATUNE</option>
+                              
+                            <option value="163920162590818304">KYUA</option>
+                              
+                            <option value="158443348363837440">KYUSO</option>
+                              
+                            <option value="154423898132185088">MACHAKOS</option>
+                              
+                            <option value="157789673886842880">MAGUGUNI</option>
+                              
+                            <option value="326570749764567040">MAKELE MUTOMO</option>
+                              
+                            <option value="157435211020238848">MAKINDU</option>
+                              
+                            <option value="164299329429831680">MAKONGENI</option>
+                              
+                            <option value="321518668074713088">MAKUTANO MWALA</option>
+                              
+                            <option value="161818060863307776">MALILI</option>
+                              
+                            <option value="157780540051685376">MASII</option>
+                              
+                            <option value="157445052640002048">MATUU</option>
+                              
+                            <option value="325908740345692160">MAWELI</option>
+                              
+                            <option value="157809168298803200">MBONDONI</option>
+                              
+                            <option value="336797926741245952">MIONDONI</option>
+                              
+                            <option value="157366522531020800">MOMBASA</option>
+                              
+                            <option value="169036218732904448">MOSA</option>
+                              
+                            <option value="258117609478684672">MTWAPA</option>
+                              
+                            <option value="157485491103465472">MUKUYUNI</option>
+                              
+                            <option value="157759283570671616">MULUTU</option>
+                              
+                            <option value="157485416415494144">MUMAANDU</option>
+                              
+                            <option value="330298397913251840">MUSA</option>
+                              
+                            <option value="157867207970258944">MUTITO-ANDEI</option>
+                              
+                            <option value="157474024794882048">MUTOMO</option>
+                              
+                            <option value="258169394645958656">MWEMBE TAYARI</option>
+                              
+                            <option value="154423898148962304">MWINGI</option>
+                              
+                            <option value="382113640066580480">NDUMONI</option>
+                              
+                            <option value="345508370557435904">NGOLIBA</option>
+                              
+                            <option value="321518779366375424">NGUNI</option>
+                              
+                            <option value="169036448425574400">NGUUTANI</option>
+                              
+                            <option value="311123058750390272">NUU</option>
+                              
+                            <option value="324901064283258880">NYS</option>
+                              
+                            <option value="270959337000665088">SALAMA</option>
+                              
+                            <option value="171470395537358848">SOFIA</option>
+                              
+                            <option value="325184736957825024">SOPHIA</option>
+                              
+                            <option value="321614473401991168">THATHA HILLS</option>
+                              
+                            <option value="154423898140573696">THIKA</option>
+                              
+                            <option value="366610581773352960">TSEIKURU</option>
+                              
+                            <option value="160324590738866176">UKIA  JUCTION</option>
+                              
+                            <option value="349948733716168704">UKUNDA</option>
+                              
+                            <option value="163656934392594432">VOI</option>
+                              
+                            <option value="332103035511635968">WAITA</option>
+                              
+                            <option value="157791145173188608">WAMUNYU</option>
+                              
+                            <option value="157414081614053376">WOTE</option>
+                              
+                            <option value="157447028928937984">YATTA</option>
+                              
+                            <option value="335410336200916992">YATTA NYS</option>
+                              
+                          </select>
+        </div>
+    )
+}
+
+export default Stages
